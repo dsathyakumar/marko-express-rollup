38c96e240d677cde8e6a40fa5a2105399c99c904
Raja Ramu <rar@ebay.com>
2019-11-09 08:24:17 -0800

    Commenting state

--- rollup.config.js
+++ rollup.config.js
@@ -1,28 +1,27 @@
-import commonjsPlugin from 'rollup-plugin-commonjs';
-import browserifyPlugin from 'rollup-plugin-browserify-transform';
-import nodeResolvePlugin from 'rollup-plugin-node-resolve';
-import markoify from 'markoify';
-import envify from 'envify';
-import path from 'path';
+import commonjsPlugin from "rollup-plugin-commonjs";
+import nodeResolvePlugin from "rollup-plugin-node-resolve";
+import replace from "rollup-plugin-replace";
+import marko from "@marko/rollup";
+import path from "path";
 
 export default {
-    entry: path.join(__dirname, 'routes/index/client.js'),
-    format: 'iife',
-    moduleName: 'app',
-    plugins: [
-        browserifyPlugin(markoify),
-        browserifyPlugin(envify),
-        nodeResolvePlugin({
-            jsnext: true,  // Default: false
-            main: true,  // Default: true
-            browser: true,  // Default: false
-            preferBuiltins: false,
-            extensions: [ '.js', '.marko' ]
-        }),
-        commonjsPlugin({
-            include: [ 'node_modules/**', '**/*.marko', '**/*.js'],
-            extensions: [ '.js', '.marko' ]
-        })
-    ],
-    dest: path.join(__dirname, './static/bundle.js')
+  input: path.join(__dirname, 'routes/index/client.js'),
+  plugins: [
+    marko(),
+    replace({ "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV) }),
+    nodeResolvePlugin({
+      mainFields: ["browser", "module", "jsnext", "main"],
+      preferBuiltins: false,
+      extensions: [".js", ".marko"]
+    }),
+    commonjsPlugin({
+      include: [],
+      extensions: [".js", ".marko"]
+    })
+  ],
+  output: {
+    name: "app",
+    format: "iife",
+    file: path.join(__dirname, './static/bundle.js')
+  }
 };
