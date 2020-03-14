1b85098c72d9792529dbc04c2b631472431833f9
dasathyakuma <dasathyakuma@ebay.com>
2020-03-14 12:00:33 -0600

    chore: sample error

--- routes/index/components/click-count/component-browser.js
+++ routes/index/components/click-count/component-browser.js
@@ -1,8 +1,12 @@
+const helpers = require('./helper');
+
 module.exports = {
     onMount() {
         this.subscribeTo(this.getEl()).on('click', this.increment);
     },
     increment() {
         alert('button clicked!');
-    }
+        this.hola();
+    },
+    ...helpers
 };
